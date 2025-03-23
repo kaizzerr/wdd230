a597f3c64846ad04d791c455733b5981805f82b2
kaizzerr <kaizzergatan@gmail.com>
2025-03-22 22:55:29 -0600

    Chamber Discover Page

--- chamber/scripts/chamberHome.js
+++ chamber/scripts/chamberHome.js
@@ -9,3 +9,24 @@ hamburger.addEventListener('click', () => {
     burger.classList.toggle('open');
     header.classList.toggle('open');
 });
+
+const visitMessage = document.getElementById('visit-message');
+const lastVisit = localStorage.getItem('lastVisit');
+
+if (lastVisit) {
+    const now = Date.now();
+    const timeDifference = now - lastVisit;
+    const daysDifference = Math.floor(timeDifference / (1000 * 3600 * 24));
+
+    if (daysDifference === 0) {
+        visitMessage.textContent = "Back so soon! Awesome!";
+    } else if (daysDifference === 1) {
+        visitMessage.textContent = "You last visited 1 day ago.";
+    } else {
+        visitMessage.textContent = `You last visited ${daysDifference} days ago.`;
+    }
+} else {
+    visitMessage.textContent = "Welcome! Let us know if you have any questions.";
+}
+
+localStorage.setItem('lastVisit', Date.now());
