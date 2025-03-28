203ca7067d8c14927ec7785dd2cf2d13c988b4d9
kaizzerr <kaizzergatan@gmail.com>
2025-03-27 18:24:52 -0600

    Form Activity

---
+++ scripts/form.js
@@ -0,0 +1,21 @@
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamburger = document.querySelector('.hamburger');
+const burger = document.querySelector('.burger');
+const header = document.querySelector('header');
+hamburger.addEventListener('click', () => {
+    header.classList.toggle('open');
+    hamburger.classList.toggle('open');
+    burger.classList.toggle('open');
+});
+
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
