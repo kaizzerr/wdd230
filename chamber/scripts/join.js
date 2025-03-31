26a0b1bae5d31a64e34f151f18ad90a27261dae4
kaizzerr <kaizzergatan@gmail.com>
2025-03-30 20:31:01 -0600

    Chamber Join

---
+++ chamber/scripts/join.js
@@ -0,0 +1,11 @@
+document.getElementById("lastModified").textContent = document.lastModified;
+
+const hamburger = document.querySelector('.hamburger');
+const burger = document.querySelector('.burger');
+const header = document.querySelector('header');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    burger.classList.toggle('open');
+    header.classList.toggle('open');
+});
