1d9ea02c9f22840a7029258b94606884321f626e
kaizzerr <kaizzergatan@gmail.com>
2025-04-02 17:18:06 -0600

    Form Activity

--- scripts/form.js
+++ scripts/form.js
@@ -19,3 +19,32 @@ rating.addEventListener('input', displayRatingValue);
 function displayRatingValue() {
     ratingvalue.innerHTML = rating.value;
 }
+
+document.getElementById("password").addEventListener('input', checkPasswordMatch);
+document.getElementById("confirmPassword").addEventListener('input', checkPasswordMatch);
+
+function checkPasswordMatch() {
+    const password = document.getElementById("password");
+    const confirmPassword = document.getElementById("confirmPassword");
+    const passwordError = document.getElementById("passwordError");
+
+    if (password.value !== confirmPassword.value) {
+        passwordError.style.display = "block";
+    } else {
+        passwordError.style.display = "none";
+    }
+}
+
+document.getElementById("email").addEventListener('input', checkEmailValidity);
+
+function checkEmailValidity() {
+    const email = document.getElementById("email");
+    const emailError = document.getElementById("emailError");
+
+    const emailPattern = new RegExp(email.pattern);
+    if (!emailPattern.test(email.value)) {
+        emailError.style.display = "block";
+    } else {
+        emailError.style.display = "none";
+    }
+}
