bd00aa592c83bb69799236e9851f5fc44aa3bea3
kaizzerr <kaizzergatan@gmail.com>
2025-04-05 23:00:32 -0600

    Home Page Development

---
+++ scripts/weather.js
@@ -0,0 +1,35 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherDesc = document.querySelector('#weather-desc');
+const weatherIcon = document.querySelector('#weather-icon');
+const weatherCaption = document.querySelector('#weather-caption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=51.04615596342734&lon=-114.09611798114804&units=metric&appid=b73f6933314bb18a4903b05abd559ed4';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayResults(data) {
+    const temp = data.main.temp;
+    const description = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+
+    currentTemp.innerHTML = `${temp}&deg;C`;
+    weatherDesc.textContent = description.charAt(0).toUpperCase() + description.slice(1);
+
+    const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+    weatherIcon.setAttribute('src', iconUrl);
+    weatherIcon.setAttribute('alt', description);
+}
+
+apiFetch();
